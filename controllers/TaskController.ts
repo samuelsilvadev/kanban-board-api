cceeb1ef762afe5469b1683c43c8e8a331359332
Samuel SIlva <samuelsilvawb@gmail.com>
2023-11-29 22:40:41 +0000

    feat: add put and get handlers (#2)

--- controllers/TaskController.ts
+++ controllers/TaskController.ts
@@ -1,4 +1,4 @@
-import { Context } from "../deps.ts";
+import { RouterMiddleware } from "../deps.ts";
 import { TaskCreateDto } from "../models/Task.ts";
 import { TaskService } from "../services/TaskService.ts";
 
@@ -9,7 +9,7 @@ export class TaskController {
     this.taskService = new TaskService();
   }
 
-  async create(context: Context) {
+  create: RouterMiddleware<string> = async (context) => {
     const { request, response } = context;
 
     if (!request.body()) {
@@ -29,5 +29,47 @@ export class TaskController {
     response.body = {
       data: task,
     };
-  }
+  };
+
+  update: RouterMiddleware<string> = async (context) => {
+    const { request, response, params } = context;
+
+    if (!params.id) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "The request must have a id",
+      };
+      return;
+    }
+
+    if (!request.body()) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "The request must have a body",
+      };
+      return;
+    }
+
+    const body: TaskCreateDto = await request.body().value;
+
+    const task = this.taskService.update(params.id, body);
+
+    response.status = 200;
+    response.body = {
+      data: task,
+    };
+  };
+
+  getAll: RouterMiddleware<string> = (context) => {
+    const { response } = context;
+
+    const tasks = this.taskService.getAll();
+
+    response.status = 200;
+    response.body = {
+      data: tasks,
+    };
+  };
 }
